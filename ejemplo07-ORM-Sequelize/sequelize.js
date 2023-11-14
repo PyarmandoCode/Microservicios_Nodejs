eb2e4e434ce303e6e69c3b7942adeac7b32be8d0
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-11-14 18:54:26 -0500

    Dia2

--- ejemplo07-ORM-Sequelize/sequelize.js
+++ ejemplo07-ORM-Sequelize/sequelize.js
@@ -1,10 +1,20 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 
-const sequelize = new Sequelize ({
-    dialect:'sqlite',
-    storage:'database.sqlite'
+// const sequelize = new Sequelize ({
+//     dialect:'sqlite',
+//     storage:'database.sqlite'
+// });
+
+// module.exports = sequelize;
+
+// const sequelize = new Sequelize('bdusers', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql' 
+// });
+const sequelize = new Sequelize('bdusers', 'admin', 'admin', {
+    host: 'localhost',
+    dialect: 'postgres'
 });
 
 module.exports = sequelize;
-

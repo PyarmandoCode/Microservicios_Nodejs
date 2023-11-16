8ec2bbaf259d79b052f6b2a337a0f05c82fb9fff
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-11-16 14:59:51 -0500

    Dia 4

---
+++ ejemplo09-JSON-SUBJSON/data.sql
@@ -0,0 +1,11 @@
+insert into authors (idautor,nombreautor,createdAt,UpdatedAt)
+values(1,"Juan Perez",now(),now())
+
+insert into books(idbook,titulo,idautor,createdAt,UpdatedAt)
+values(1,"Libro1",1,now(),now())
+
+insert into books(idbook,titulo,idautor,createdAt,UpdatedAt)
+values(2,"Libro1",1,now(),now())
+
+#Api para listar Autores-Books
+http://localhost:3000/api/authoresbooks

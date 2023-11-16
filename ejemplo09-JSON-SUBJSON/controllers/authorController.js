8aa6cc5d3fd079308f9cd71afee7669841ba8101
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-11-16 15:44:05 -0500

    Dia4

--- ejemplo09-JSON-SUBJSON/controllers/authorController.js
+++ ejemplo09-JSON-SUBJSON/controllers/authorController.js
@@ -1,7 +1,32 @@
 import Author from '../models/authorModels.js';
 import Books from '../models/booksModels.js';
 
-export const Author_Books = async (req,res) => {
-    const author_books = await Author.findAll({include:Books});
-    res.status(200).json(author_books);
-};
+module.exports = {
+    Author_Books: async (req, res) => {
+        try {
+            const author_books = await Author.findAll({ include: Books });
+            res.json(author_books);
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error en el Server");
+        }
+    },
+    createauthor: async (req, res) => {
+        const { idautor, nombreautor } = req.body;
+        try {
+            const author = await Author.create({ idautor, nombreautor });
+            res.json(author);
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error en el Server");
+
+        }
+    }
+}
+
+// export const Author_Books = async (req,res) => {
+//     const author_books = await Author.findAll({include:Books});
+//     res.status(200).json(author_books);
+
--- ejemplo09-JSON-SUBJSON/router/authorRoute.js
+++ ejemplo09-JSON-SUBJSON/router/authorRoute.js
@@ -1,9 +1,11 @@
 import Router from "express";
-import {Author_Books} from "../controllers/authorController.js";
+import {Author_Books } from "../controllers/authorController.js";
+
 
 const router = Router();
 
 router.get("/authoresbooks",Author_Books);
+//router.post("/createauthor" ,CreateAuthor)
 
 
 export default router;
